559c3f224e0bffd3b663dc431b5bd40119767cd5
Tu Nguyen <nvminhtu@gmail.com>
2022-11-29 00:20:42 +0700

    of rxjs

--- side-menu/src/app/pages/rxjs/rxjs.page.ts
+++ side-menu/src/app/pages/rxjs/rxjs.page.ts
@@ -9,14 +9,28 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./rxjs.page.scss'],
 })
 export class RxjsPage implements OnInit {
-  nums = of(1, 2, 3);
-  squareValues = map((val: number) => val * val);
-  squaredNums = this.squareValues(this.nums);
-  // squaredNums.subscribe(x => console.log(x));
+  nums: any;
+  squareValues: any;
+  squaredNums!: any;
+
+
+  data: number[] = [];
 
   constructor() {}
-  ofGenerateFunc() {}
+
+  ofGenerateFunc() {
+    // test rxJS: Of
+    this.nums = of(1, 2, 3); // complete: 'complete' - output: 1,2,3
+    this.squareValues = map((val: number) => val * val); // function này : sử dụng hàm map thông thường
+    // this.squaredNums = this.squareValues(this.nums);  // chạy hàm map cho nums: là 1 Observable từ tập giá trị Primittives
+    // this.squaredNums.subscribe((x:number) => console.log(x)); // . subscribe(observer)
+    // this.data = this.squaredNums.subscribe((x: number)=> console.log(x));
+  
+    const source$ = of(1, 2, 3, 4, 5);  //output: 1,2,3,4,5
+    const subscribe = source$.subscribe((val:number) => this.data.push(val));
+  
+  }
   ngOnInit() {
-    this.squaredNums.subscribe((x) => console.log(x));
+    this.ofGenerateFunc();
   }
 }
